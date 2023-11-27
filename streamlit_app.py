fd97defbbad80afe7ede674a117a11d352930efc
Fernando Lopez Rubio <fernando.lr@ibm.com>
2023-11-27 14:37:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import os
 import cv2
 from streamlit_image_select import image_select
 import urllib.request
+import random
 
 
 
@@ -17,6 +18,23 @@ def get_image_path(img):
         img_file.write(img.getbuffer())
     return file_path
 
+def sp_noise(image,prob):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    output = np.zeros(image.shape,np.uint8)
+    thres = 1 - prob 
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
 
 st.title('Autoencode image denoiser')
 
@@ -46,13 +64,13 @@ st.write(selected_image)
 
 req = urllib.request.urlopen(selected_image)
 arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-opencv_image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-opencv_image = cv2.cvtColor(opencv_image , cv2.COLOR_BGR2RGB)
-# ReSize
-resized = cv2.resize(opencv_image,dsize=(256,256), interpolation=cv2.INTER_CUBIC)
-# ReScale Values
-resized = resized / 255
-st.image(resized)
+selected_image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+selected_image = cv2.cvtColor(selected_image , cv2.COLOR_BGR2RGB)
+
+noisy_image = sp_noise(selected_image, 0.1)
+
+st.image(selected_image)
+st.image(noisy_image)
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
