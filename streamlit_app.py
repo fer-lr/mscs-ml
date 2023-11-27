0c35dea50c04ca3b28829c56e6b33dbd412af85e
Fernando Lopez Rubio <fernando.lr@ibm.com>
2023-11-27 14:39:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,12 +39,11 @@ def sp_noise(image,prob):
 st.title('Autoencode image denoiser')
 
 
-# Using "with" notation
-with st.sidebar:
-    st.markdown("Hello world")
+# # Using "with" notation
+# with st.sidebar:
+#     st.markdown("Hello world")
 
-
-
+st.write("BRIEF DESCRIPTION")
 st.image("https://raw.githubusercontent.com/fer-lr/mscs-ml/main/images/autoencoder_schema.jpg")
 
 selected_image = image_select("Label", ["https://raw.githubusercontent.com/fer-lr/mscs-ml/main/images/picker/15240.jpg",
@@ -60,40 +59,17 @@ selected_image = image_select("Label", ["https://raw.githubusercontent.com/fer-l
                                         "https://raw.githubusercontent.com/fer-lr/mscs-ml/main/images/picker/15250.jpg",
                                         ])
 
-st.write(selected_image)
-
 req = urllib.request.urlopen(selected_image)
 arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
 selected_image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
 selected_image = cv2.cvtColor(selected_image , cv2.COLOR_BGR2RGB)
-
 noisy_image = sp_noise(selected_image, 0.1)
 
-st.image(selected_image)
-st.image(noisy_image)
+col1, col2 = st.columns(2)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
-
-st.subheader('Raw data')
-st.write(data)
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+with col1: 
+    st.image(selected_image)
+    st.image(noisy_image)
 
 
 uploaded_file = st.file_uploader("Choose an image",type=['jpg'])
@@ -107,9 +83,3 @@ if uploaded_file is not None:
     # ReScale Values
     resized = resized / 255
     st.image(resized)
-
-st.bar_chart(hist_values)
-
-st.subheader('Map of all pickups')
-
-st.map(data)
