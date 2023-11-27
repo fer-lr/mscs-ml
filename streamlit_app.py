b4969ec3e369f7889c87bccbc8a75e6af547ec85
Fernando Lopez Rubio <fernando.lr@ibm.com>
2023-11-27 14:52:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ def sp_noise(image,prob):
 def mse(imageA, imageB):
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
 
 st.title('Autoencode image denoiser')
 
