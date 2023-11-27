383bd9fe73391e8ec93bdfa4d1df281c18388ab5
Fernando Lopez Rubio <fernando.lr@ibm.com>
2023-11-27 13:58:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,9 +50,9 @@ if uploaded_file is not None:
     bytes_data = get_image_path(uploaded_file)
     st.image(bytes_data)
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
+    opencv_image = cv2.imdecode(file_bytes, 3)
     # ReSize
-    resized = cv2.resize(opencv_image,dsize=(224,224), interpolation=cv2.INTER_CUBIC)
+    resized = cv2.resize(opencv_image,dsize=(256,256), interpolation=cv2.INTER_CUBIC)
     # ReScale Values
     resized = resized / 255
     st.image(resized)
