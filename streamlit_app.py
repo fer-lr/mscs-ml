f49313b65f5d2ac66640e8c0a8bcb14d28d227c3
Fernando Lopez Rubio <fernando.lr@ibm.com>
2023-11-28 12:27:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,7 +159,7 @@ st.divider()
 st.subheader("The process")
 st.markdown("#### Image selection")
 
-col3, col4 = st.columns([3,1],gap="medium")
+col3, col4 = st.columns([4,2],gap="medium")
 
 with col3:
     st.write("A sample of **3,004 256x256** images of celebrity faces from a bank of 30,000 was selected to train and validate the model.")
