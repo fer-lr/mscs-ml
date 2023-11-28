71e4c557657ad9f97b4ef58fa84dff34b6d17c50
Fernando Lopez Rubio <fernando.lr@ibm.com>
2023-11-27 20:40:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ st.write(tf.__version__)
 st.write("BRIEF DESCRIPTION")
 st.image("https://raw.githubusercontent.com/fer-lr/mscs-ml/main/images/autoencoder_schema.jpg")
 
-model = load_model("model/model200_2-15.keras")
+model = load_model("model/model200-2-15.keras")
 
 #pickled_model = pickle.load(open('model/200_2.pkl', 'rb'))
 
